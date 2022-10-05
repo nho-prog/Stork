651ba2d97500be144a95687e671616322e18feff
meenamit <kumara82523@gmail.com>
2022-10-05 23:57:36 +0530

    .vscode/settings.json worker.c

--- esb_app/src/esb/worker.c
+++ esb_app/src/esb/worker.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
 #include <unistd.h>
 #include "esb.h"
+
 
 int fetch_new_request_from_db(bmd *request)
 {
@@ -8,7 +11,12 @@ int fetch_new_request_from_db(bmd *request)
      * TODO: query the DB for this, and populate the 
      * request pointer with the requests.
      */
+
     printf("Checking for new requests in esb_requests table.\n");
+    if (request == NULL){
+        return -1;
+    }
+
     return 1; // 1 => OK, -1 => Errors
 }
 
@@ -18,15 +26,79 @@ int fetch_new_request_from_db(bmd *request)
 void *poll_database_for_new_requets(void *vargp)
 {
     // Step 1: Open a DB connection
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    rc = sqlite3_open("test.db", &db);
+    if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return(0);
+    } else {
+      fprintf(stdout, "Opened database successfully\n");
+    }
+    char sql[];
+
+    sql="CREATE TABLE ESB_REQUEST("
+   "ID INT PRIMARY KEY NOT NULL",
+   "SENDER_ID VARCHAR(45) NOT NULL",
+   "DEST_ID VARCHAR(45) NOT NULL",
+   "MESSAGE_TYPE VARCHAR(45) NOT NULL",
+   "REFERENCE_ID VARCHAR(45) NOT NULL",
+   "MESSAGE_ID VARCHAR(45) NOT NULL",
+   "RECIEVED_ON DATETIME NOT NULL",
+   "DATA_LOCATION TEXT NOT NULL",
+   "STATUS VARCHAR(20) NOT NULL",
+   "STATUS_DETAILS TEXT NOT NULL);";
+
+    sql="CREATE TABLE ROUTES("
+   "ROUTE_ID INT PRIMARY KEY NOT NULL",
+   "SENDER VARCHAR(45) TEXT NOT NULL",
+   "DESTINATION VARCHAR(45) TEXT NOT NULL",
+   "MESSAGE_TYPE VARCHAR(45) NOT NULL",
+   "IS_ACTIVE BIT(1) NOT NULL);";
+
+    sql="CREATE TABLE TRANSFORM_CONFIG("
+   "ID INT PRIMARY KEY NOT NULL",
+   "ROUTE_ID INT MOT NULL",
+   "CONFIG_KEY VARCHAR(45) NOT NULL",
+   "CONFIG_VALUE TEXT NOT NULL);";
+
+    sql="CREATE TABLE TRANSPORT_CONFIG("
+   "ID INT PRIMARY KEY NOT NULL",
+   "ROUTE_ID INT NOT NULL",
+   "CONFIG_KEY VARCHAR(45) NOT NULL",
+   "CONFIG_VALUE TEXT NOT NULL);";
+
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+    } else {
+      fprintf(stdout, "Tables created successfully\n");
+    }
+    sqlite3_close(db);
+
     int i = 0;
     while (i < 99)
     {
         i++;
+        vargp[i];
+
         /**
          * Step 2: Query the esb_requests table to see if there
          * are any newly received BMD requets.
          */
         bmd req;
+        /* Get the route_id to handle the request */
+        int route_id = get_active_route_id(req->sender,
+                                           req->destination,
+                                           req->message_type);
+
+        /* Get transformation and transportation configuration */
+        transform_t *transform = fetch_transform_config(route_id);
+        transport_t *transport = fetch_transport_config(route_id);
+
         /**
          * Step 3:
          */
